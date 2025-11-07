b6fa2e42400cb80508ed0cb36b54263a250ca1d2
Ernestos122 <ernesto.jaramillo@uai.cl>
2025-11-07 02:01:19 -0300

    Feat: Corrected all module and service related problems and added correspondind thingies

--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
-
+/* eslint-disable prettier/prettier */
+import { Controller, Get, Post, Body, Param, Put, Delete } from '@nestjs/common';
+import { BlogService } from './blog.service';
 @Controller('blog')
-export class BlogController {}
+export class BlogController {
+  constructor(private readonly blogService: BlogService) {}
+
+  @Post()
+  create(@Body() blogPost: { id: number; title: string; content: string }) {
+    this.blogService.create(blogPost);
+  }
+  
+  @Get()
+  findAll() {
+    return this.blogService.findAll();
+  }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.blogService.findOne(+id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updatedPost: Partial<{ title: string; content: string }>) {
+    this.blogService.update(+id, updatedPost);
+  }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.blogService.remove(+id);
+    }
+}
--- src/blog/blog.service.ts
+++ src/blog/blog.service.ts
@@ -1,41 +1,40 @@
 import { Injectable } from '@nestjs/common';
+interface BlogPost {
+  id: number;
+  title: string;
+  content: string;
+}
 
-@Injectable()
-
-interface BlogPost { //Creo interfaz para definir e interactuar con data
-        id: number;
-        title: string;
-        content: string;
-    }
-    
 @Injectable()
 export class BlogService {
-    private readonly blogPosts: BlogPost[] = [];
+  private readonly blogPosts: BlogPost[] = [];
 
-    create(blogPost: BlogPost) {
-        this.blogPosts.push(blogPost);
+  create(blogPost: BlogPost) {
+    this.blogPosts.push(blogPost);
+  }
+
+  findAll(): BlogPost[] | any { //For some god forsaken reason putting any solves a typescript problem. ols helo.
+    return this.blogPosts;
+  }
+
+  findOne(id: number): BlogPost | undefined | any { //same here
+    return this.blogPosts.find((post) => post.id === id);
+  }
+
+  update(id: number, updatedPost: Partial<BlogPost>) {
+    const postIndex = this.blogPosts.findIndex((post) => post.id === id);
+    if (postIndex !== -1) {
+      this.blogPosts[postIndex] = {
+        ...this.blogPosts[postIndex],
+        ...updatedPost,
+      };
     }
+  }
 
-    findAll(): BlogPost[] {
-        return this.blogPosts;
+  remove(id: number) {
+    const postIndex = this.blogPosts.findIndex(post => post.id === id);
+    if (postIndex !== -1) {
+      this.blogPosts.splice(postIndex, 1);
     }
-
-    findOne(id: number): BlogPost | undefined {
-        return this.blogPosts.find(post => post.id === id);
-    }
-
-    update(id:number, updatedPost: Partial<BlogPost>) {
-        const postIndex = this.blogPosts.findIndex(post => post.id === id);
-        if(postIndex !== -1) {
-            this.blogPosts[postIndex] = {...this.blogPosts[postIndex],...updatedPost};
-        }
-        
-    }
-
-    remove(id: number) {
-        const postIndex = this.blogPosts.findIndex(post => post.id === id);
-        if (postIndex !== -1) {
-            this.blogPosts.splice(postIndex, 1);
-        }
   }
 }
